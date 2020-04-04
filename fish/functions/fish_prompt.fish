88ac401793c2e474636cdbab5c62c2359eb633b9
Lois Tatis <ltatis1992@gmail.com>
2020-04-03 22:30:32 -0400

    update to fish prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -86,31 +86,3 @@ function fish_prompt
 
     echo -n -s $arrow ' '$cwd $repo_info $normal ' '
 end
-  switch "$USER"
-        case root toor
-            if set -q fish_color_cwd_root
-                set color_cwd $fish_color_cwd_root
-            else
-                set color_cwd $fish_color_cwd
-            end
-            set suffix '#'
-        case '*'
-            set color_cwd $fish_color_cwd
-            set suffix '$'
-    end
-
-    # PWD
-    set_color $color_cwd
-    echo -n (prompt_pwd)
-    set_color normal
-
-    printf '%s ' (__fish_vcs_prompt)
-
-    if not test $last_status -eq 0
-        set_color $fish_color_error
-        echo -n "[$last_status] "
-        set_color normal
-    end
-
-    echo -n "$suffix "
-end
